383f4ce6dc14c9afd544f1f21e9e0cd929642671
JABRI MAISSA <l38117419@auchanlux.lgn>
2017-11-03 14:07:18 +0100

    refactoring code sturcture

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var gulpSequence = require('gulp-sequence');
 var browserify = require('gulp-browserify');
 
 gulp.task('browserify', function() {
-  return gulp.
-    src('./index.js').
-    pipe(browserify()).
-    pipe(gulp.dest('./bin'));
+  	return gulp.
+	    src('./client/index.js').
+	    pipe(browserify()).
+	    pipe(gulp.dest('./bin'));
 });
 
+gulp.task('copy', function() {
+  	return gulp.
+	    src(['./client/templates/*.html']).
+	    pipe(gulp.dest('./bin/templates/'));
+});
+
+gulp.task('clean', function () {
+    return gulp.src('bin')
+        .pipe(clean());
+});
+
+gulp.task('build', gulpSequence('clean', ['browserify', 'copy']));
+
 gulp.task('watch', function() {
-  gulp.watch(['./*.js'], ['browserify']);
+  gulp.watch(['./*.js', './*.html', './client/*.js', './client/components/*.js'], ['browserify', 'copy']);
 });
